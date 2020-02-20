f0211add65fe8ed918ffb208ff68bc8e30b976c7
17wh1a0598 <17wh1a0598@bvrithyderabad.edu.in>
2020-02-20 22:39:11 +0530

    commit.

---
+++ hometask3/src/main/java/epam/hometask4/Main.java
@@ -0,0 +1,15 @@
+package epam.hometask4;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SimpleCompundInterest.Calculator();
+
+		CostOfConstruction.Evaluate();
+
+
+	}
+
+}
